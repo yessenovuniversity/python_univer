aaa569d83b79a406edf1a3c72511bddb6701010a
Nauryzbek Aitbayev <nauryzbek.aitbayev@yu.edu.kz>
2020-09-11 14:23:32 +0500

    Fixed primary_Key to primary_key

--- univer_db/models.py
+++ univer_db/models.py
@@ -207,7 +207,7 @@ class Attendance(Base):
 
     __tablename__ = 'univer_attendance'
 
-    date = Column('att_date', Date, primary_Key=True)
+    date = Column('att_date', Date, primary_key=True)
     grade = Column('ball', Float)
     was = Column(Boolean)
     student_id = Column(ForeignKey('univer_students.students_id'), primary_key=True)
