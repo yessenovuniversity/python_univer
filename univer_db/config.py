3907ee15559743576dd692d03a25132d879b60ac
Nauryzbek Aitbayev <nauryzbek.aitbayev@yu.edu.kz>
2020-11-06 22:43:45 +0500

    Added driver in config

--- univer_db/config.py
+++ univer_db/config.py
@@ -1,5 +1,10 @@
+class DRIVER:
+    FREETDS = 'FreeTDS'
+    ODBC_17 = 'ODBC+Driver+17+for+SQL+Server'
+
 class Config:
-    def __init__(self, host, user, password):
+    def __init__(self, host, user, password, driver=DRIVER.FREETDS):
         self.host = host
         self.user = user
         self.password = password
+        self.driver = driver
