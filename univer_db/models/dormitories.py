c9251fe164db5aaf027b28443fa2e0848f8561a4
Nauryzbek Aitbayev <nauryzbek.aitbayev@yu.edu.kz>
2020-12-03 10:30:49 +0500

    Added StudentDormitoryRoomSettleHistory

--- univer_db/models/dormitories.py
+++ univer_db/models/dormitories.py
@@ -61,3 +61,71 @@ class DormitoryRoom(Base):
     
     def __str__(self):
         return self.number_ru
+
+
+class DormitoryApplicant(Base):
+    """
+    Модель "Заявление общежития"
+    """
+    __tablename__ = 'univer_dormitory_applicant'
+
+    # Идентификатор
+    id = Column('dormitory_applicant_id', Integer, primary_key=True)
+
+    def __repr__(self):
+        return '<DormitoryApplicant {} (id={})>'.format(self, self.id)
+    
+    def __str__(self):
+        return str(self.id)
+
+
+class StudentDormitoryRoomSettleHistory(Base):
+    """
+    Модель "История проживания в комнате общежития студентом"
+    """
+    __tablename__ = 'univer_student_dormitory_room_settle_history'
+
+    # Идентификатор
+    id = Column('history_id', Integer, primary_key=True)
+
+    # Студент
+    student_id = Column(ForeignKey('univer_students.students_id'))
+    student = relationship('Student')
+
+    # Комната общежития
+    dormitory_room_id = Column(ForeignKey('univer_dormitory_rooms.dormitory_room_id'))
+    dormitory_room = relationship('DormitoryRoom')
+
+    # Дата и время
+    date = Column('history_date', DateTime)
+
+    # Заявление общежития
+    dormitory_applicant_id = Column(ForeignKey('univer_dormitory_applicant.dormitory_applicant_id'))
+    dormitory_applicant = relationship('DormitoryApplicant')
+
+    # Общежитие
+    dormitory_id = Column(ForeignKey('platonus_univer_dormitories.dormitoryID'))
+    dormitory = relationship('Dormitory')
+
+    # Тип приказа
+    order_type = Column(Integer)
+
+    # Дата и время приказа
+    order_date = Column(DateTime)
+
+    # Статус приказа
+    order_status = Column(Integer)
+
+    def __repr__(self):
+        return '<StudentDormitoryRoomSettleHistory {} (id={} student_id={} dormitory_room_id={} date={} dormitory_applicant_id={} order_type={}'.format(
+            self,
+            self.id,
+            self.student_id,
+            self.dormitory_room_id,
+            self.date,
+            self.dormitory_applicant_id,
+            self.order_type
+        )
+
+    def __str__(self):
+        return str(self.student)
